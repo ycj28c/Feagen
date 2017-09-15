b1a6c87ebcf3a54f05102a2fa449c1f5031c5fd7
Ralph <ycj28c@gmail.com>
2017-09-14 18:51:06 -0700

    no message

--- example/src/test/java/test/github/ui/steps/advance/LoginPageStep.java
+++ example/src/test/java/test/github/ui/steps/advance/LoginPageStep.java
@@ -3,9 +3,10 @@ package test.github.ui.steps.advance;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
-import io.github.pageobject.LoginPage;
 import io.github.pageobject.GoogleSearchResultPage;
+import io.github.pageobject.LoginPage;
 import test.github.ui.steps.AbstractStepDefinition;
 
 public class LoginPageStep extends AbstractStepDefinition{
@@ -15,18 +16,8 @@ public class LoginPageStep extends AbstractStepDefinition{
 		driver.get("https://www.google.com");
 		return loginPage;
 	}
-
-//	@When("^enter \"([^\"]*)\" in to the search bar$")
-//	public LoginPage enter_in_to_the_search_bar(String arg1) throws Throwable {
-//		// Find the text input element by its name
-//		WebElement element = driver.findElement(By.name("q"));
-//
-//		// Enter something to search for
-//		element.sendKeys(arg1);
-//		return loginPage;
-//	}
 	
-	@When("^enter feagen in to the search bar$")
+	@And("^enter feagen in to the search bar$")
 	public LoginPage enter_feagen_in_to_the_search_bar() throws Throwable {
 		// Find the text input element by its name
 		WebElement element = driver.findElement(By.name("q"));
@@ -36,7 +27,7 @@ public class LoginPageStep extends AbstractStepDefinition{
 		return loginPage;
 	}
 
-	@When("^click the search button$")
+	@And("^click the search button$")
 	public GoogleSearchResultPage click_the_search_button() throws Throwable {
 		WebElement element = driver.findElement(By.name("q"));
 		
